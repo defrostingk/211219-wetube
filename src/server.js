a46450d8fffb1647328f397823639cbd79a021f3
Haedong Kim <haedong1111@gmail.com>
2022-02-25 21:25:42 +0900

    crossorigin

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const logger = morgan("dev");
 
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "credentialless");
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
